6505517c5dfc0dfa9d2dce312372222f4d0098e8
Kazi Mamun <kazimamun716@gmail.com>
2022-07-31 20:51:36 +0600

    order delete route added

--- controllers/manufacturerControlers.js
+++ controllers/manufacturerControlers.js
@@ -8,25 +8,61 @@ const client = new MongoClient(uri, {
 });
 
 let serviceCollection;
+let orderCollection;
 
 module.exports = {
   dbConnection: function (req, res, next) {
     client.connect((err) => {
       const db = client.db("tools-manufacturer");
       serviceCollection = db.collection("services");
+      orderCollection = db.collection("orders");
       next();
     });
   },
+  //get all services
   services: async function (req, res) {
     const query = {};
     const cursor = serviceCollection.find(query);
     const services = await cursor.toArray();
-    console.log(services);
     res.send(services);
   },
+  //get service by id
   serviceById: async function(req, res){
     const id = req.params.id;
     const service = await serviceCollection.findOne({_id: ObjectId(id)});
     res.send(service)
+  },
+  //for order
+  createOrder: async (req, res)=>{
+    const order = req.body;
+    const setOrder = await orderCollection.insertOne(order);
+    res.send(setOrder)
+  },
+  //available qty for services
+  availableQty: async(req, res)=>{
+    const orderdQty = req.body.qty;
+    const serviceId = req.body.serviceId;
+    const query = {_id: ObjectId(serviceId)};
+    const orderedService = await serviceCollection.findOne(query);
+    const newQty = orderedService.available-orderdQty;
+    const filter = { _id: ObjectId(serviceId) };
+    const options = { upsert: true };
+    const updateDoc = {
+      $set:{available: newQty}
+    }
+    const result = await serviceCollection.updateOne(filter, updateDoc, options);
+    res.send(result)
+  },
+  //get order by user email
+  getOrderById: async(req, res)=>{
+    const user = req.params.email;
+    const orders = await orderCollection.find({email: user}).toArray();
+    res.send(orders)
+  },
+  deleteOrderById: async(req, res)=>{
+    const order = req.params.id;
+    const query={_id: ObjectId(order)}
+    const result = await orderCollection.deleteOne(query);
+    res.send(result)
   }
 };
--- routes/manufacturerRoutes.js
+++ routes/manufacturerRoutes.js
@@ -1,15 +1,34 @@
-const { services, dbConnection, serviceById } = require('../controllers/manufacturerControlers');
+const {
+  services,
+  dbConnection,
+  serviceById,
+  createOrder,
+  availableQty,
+  getOrderById,
+  deleteOrderById,
+} = require("../controllers/manufacturerControlers");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
 //database connection
-router.all('*', dbConnection)
+router.all("*", dbConnection);
 
 //get all services
-router.get('/services', services)
+router.get("/services", services);
+
+//get order with available quantity
+router.put("/availableQty", availableQty);
 
 //get single service by id
-router.get('/service/:id', serviceById)
+router.get("/service/:id", serviceById);
 
+//post order
+router.post("/createOrder", createOrder);
+
+//get order by user
+router.get('/orders/:email', getOrderById)
+
+//order delete by id
+router.delete('/orders/:id', deleteOrderById)
 
 module.exports = router;
